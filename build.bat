16550496904861ee74c701c06369453e31cce7b1
Matthew McConnell <maca134@googlemail.com>
2016-09-28 19:36:58 +0100

    Updated build to include tester exe and zip package

--- build.bat
+++ build.bat
@@ -7,7 +7,11 @@ mkdir @ExileLootDrop\addons
 %MSBUILD% src\ExileLootDrop.sln /property:Configuration=release /target:Rebuild /verbosity:normal /nologo
 copy src\ExileLootDrop\bin\Release\ExileLootDrop.dll @ExileLootDrop
 copy src\ExileLootDrop\bin\Release\ExileLootDrop.cfg @ExileLootDrop
+copy src\ExileLootDropTester\bin\Release\ExileLootDropTester.exe @ExileLootDrop
 copy LICENSE.txt @ExileLootDrop
 copy README.md @ExileLootDrop
 
 %MAKEPBO% -U -W -P -@=ExileLootDrop sqf @ExileLootDrop\addons\ExileLootDrop.pbo
+del @ExileLootDrop.zip
+powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('@ExileLootDrop', '@ExileLootDrop.zip'); }"
+pause
